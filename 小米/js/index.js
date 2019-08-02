874a543fb2cddb0b8475557f70cdd65391fe8263
guoqiang <849348323@qq.com>
2019-08-02 20:14:31 +0800

    加了一点动画

--- 小米/js/index.js
+++ 小米/js/index.js
@@ -4,3 +4,21 @@ new Vue({
 		car:["手机 电话卡","电视 盒子","笔记本 平板","家电 插线板","出行 穿戴","智能 路由器","电源 配件","健康 儿童","耳机 音响","生活 箱包"],
 	}
 })
+
+
+$(".dongHua1").mouseover(function(){
+	// var $index=$(this).index();
+	// $(".show").eq($index).show()
+	// $(".dongHua").mouseout(function(){
+	// 	$(".show").eq($index).hide()
+	// })
+	$this = $(this);
+	// console.log($this);
+	// console.log($this.children('.mask'))
+	$this.children('.mask').addClass('show');
+	$(".dongHua1").mouseout(function () { 
+	$this.children('.mask').removeClass('show');
+		
+	})
+
+})
